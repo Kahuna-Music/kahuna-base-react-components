37621d6d9d81a3ca7522a369c95f91d1d9495826
barin <barin.ozmen@xerini.co.uk>
2024-03-08 10:43:18 +0300

    1.0.6

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1,2 +1,2 @@
 import{jsx as t}from"react/jsx-runtime";var n=function(n){return t("button",{disabled:n.disabled,"data-testid":"".concat(n.testIdPrefix),className:"",onClick:function(){},children:t("span",{children:n.label})})},e=function(n){return t("span",{children:n.text})};export{n as KButton,e as KSpan};
-//# sourceMappingURL=index.js.map
+//# sourceMappingURL=index.esm.js.map
