211f98026e0b313231bcf4d7a0c61d7a259e0b01
barin <barin.ozmen@xerini.co.uk>
2024-05-06 10:17:36 +0300

    build npm

--- dist/components/KInput/KInput.stories.d.ts
+++
@@ -1,6 +0,0 @@
-declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react").ReactRenderer, import("./KInput").KInputProps>;
-export default _default;
-export declare const KInputPrimary: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, import("./KInput").KInputProps>;
-export declare const KInputLeftIcon: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, import("./KInput").KInputProps>;
-export declare const KInputRightIcon: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, import("./KInput").KInputProps>;
-export declare const KInputLeftRightIcon: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, import("./KInput").KInputProps>;
