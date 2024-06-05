845bb78996c59d911937486f7f30ccd705eba633
barin <barin.ozmen@xerini.co.uk>
2024-06-05 12:50:03 +0300

    0.2.10

--- dist/components/KDropdownToggle/KDropdownToggle.stories.d.ts
+++
@@ -1,7 +0,0 @@
-declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react").ReactRenderer, import("./KDropdownToggle").KDropdownProps>;
-export default _default;
-export declare const KDropdownToggleSingle: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, import("./KDropdownToggle").KDropdownProps>;
-export declare const KDropdownToggleMulti: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, import("./KDropdownToggle").KDropdownProps>;
-export declare const KDropdownToggleLeftIcon: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, import("./KDropdownToggle").KDropdownProps>;
-export declare const KDropdownToggleRightIcon: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, import("./KDropdownToggle").KDropdownProps>;
-export declare const KDropdownToggleLeftRightIcon: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, import("./KDropdownToggle").KDropdownProps>;
