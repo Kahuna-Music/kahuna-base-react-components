cad0a362ec320c0379e935fb2e86e2202e6059b0
barin <barin.ozmen@xerini.co.uk>
2024-07-22 19:15:25 +0300

    k logo updates

--- src/components/KLogo/KLogo.tsx
+++ src/components/KLogo/KLogo.tsx
@@ -11,6 +11,8 @@ import LogoWhite from "../../assets/logo-small.svg"
 //@ts-ignore
 import LogoWhiteHovered from "../../assets/logo-small-hovered.svg"
 //@ts-ignore
+import UnionSvg from "../../assets/union.svg"
+//@ts-ignore
 import LogoGray from "../../assets/logo-gray.svg"
 import KTitleSpan from "../KTitleSpan"
 import KSpan from "../KSpan"
@@ -19,15 +21,15 @@ export interface KLogoProps {
   width?: number
   height?: number
   borderRadius?: number
-  primaryText?: string
+  primaryTextVisible?: boolean
   secondaryText?: string
   logoColor?: string
   hoverEnabled?: boolean
-  primaryTextColor?: string
   secondaryTextColor?: string
-  primaryTextFontSize?: number
   secondaryTextFontSize?: number
   hideIcon?: boolean
+  primaryTextWidth?: number
+  primaryTextHeight?: number
 }
 
 const KLogo: React.FC<KLogoProps> = (props) => {
@@ -40,9 +42,10 @@ const KLogo: React.FC<KLogoProps> = (props) => {
   const hoverEnabled = props.hoverEnabled || false
   const hideIcon = props.hideIcon || false
 
-  const primaryTextColor = props.primaryTextColor || "#111"
+  const primaryTextVisible = props.primaryTextVisible || false
+  const primaryTextWidth = props.primaryTextWidth || 76
+  const primaryTextHeight = props.primaryTextHeight || 16
   const secondaryTextColor = props.secondaryTextColor || "#000"
-  const primaryTextFontSize = props.primaryTextFontSize || 20
   const secondaryTextFontSize = props.secondaryTextFontSize || 14
 
   const [onHover, setOnHover] = useState(false)
@@ -58,18 +61,12 @@ const KLogo: React.FC<KLogoProps> = (props) => {
           onMouseLeave={() => setOnHover(false)}
         />
       )}
-      {props.primaryText && (
-        <div className={"ml-2 mr-2 flex items-center"}>
-          <KTitleSpan
-            fontSize={primaryTextFontSize}
-            lineHeight={`${primaryTextFontSize}px`}
-            text={props.primaryText}
-            color={primaryTextColor}
-            bold={true}
-          />
+      {primaryTextVisible && (
+        <div className={"mr-2 flex items-center"} style={{marginLeft: -8}}>
+          <img src={UnionSvg} alt={"kahuna-union"} style={{ width: primaryTextWidth, height: primaryTextHeight }} />
         </div>
       )}
-      {(props.primaryText && props.secondaryText) && <img src={VectorIcon} alt="vector" />}
+      {primaryTextVisible && props.secondaryText && <img src={VectorIcon} alt="vector" />}
       {props.secondaryText && (
         <div className={"pl-2 flex items-center"}>
           <KSpan fontSize={secondaryTextFontSize} text={props.secondaryText} color={secondaryTextColor} />
