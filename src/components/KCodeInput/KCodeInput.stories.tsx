1cdeb7fe39a84206518313d56ef83bc3f5bda351
unknown <evrentank@gmail.com>
2024-07-24 20:20:34 +0300

    save point

--- src/components/KCodeInput/KCodeInput.stories.tsx
+++ src/components/KCodeInput/KCodeInput.stories.tsx
@@ -1,84 +1,24 @@
 import { Meta, StoryFn } from "@storybook/react"
-import KInput from "./KCodeInput"
+import KCodeInput from "./KCodeInput"
 // @ts-ignore
 import TracksIcon from "../../assets/tracks.svg"
 import { KeyboardEvent } from "react"
 
 export default {
-  title: "ReactComponentLibrary/KInput",
-  component: KInput,
+  title: "ReactComponentLibrary/KCodeInput",
+  component: KCodeInput,
   parameters: {
     layout: "centered"
   }
-} as Meta<typeof KInput>
+} as Meta<typeof KCodeInput>
 
-const Template: StoryFn<typeof KInput> = (args) => <KInput {...args} />
+const Template: StoryFn<typeof KCodeInput> = (args) => <KCodeInput {...args} />
 
 export const KInputRange = Template.bind({})
 KInputRange.args = {
   onChange: (value: string) => {},
-  placeholder: "Placeholder...",
   hoverBackground: "white",
-  type: "range",
-  leftIcon: TracksIcon,
-  rightIcon: TracksIcon
-}
-
-export const KInputCheckbox = Template.bind({})
-KInputCheckbox.args = {
-  onChange: (value: string) => {},
-  placeholder: "Placeholder...",
-  hoverBackground: "white",
-  type: "checkbox",
-  leftIcon: TracksIcon,
-  rightIcon: TracksIcon,
-  checked: false
-}
-
-export const KInputPrimary = Template.bind({})
-KInputPrimary.args = {
-  onChange: (value: string) => {
-    console.log("value:", value)
-  },
-  onKeyDown: (event: KeyboardEvent) => {
-    if (event.key === "Enter") {
-      console.log("Enter is clicked and our value is:", event.currentTarget)
-    }
-  },
-  placeholder: "Placeholder...",
-  hoverBackground: "white"
-}
-
-export const KInputLeftIcon = Template.bind({})
-KInputLeftIcon.args = {
-  onChange: (value: string) => {},
-  placeholder: "Placeholder...",
-  leftIcon: TracksIcon,
-  leftIconClick: () => {
-    alert("left icon clicked")
-  }
-}
-
-export const KInputRightIcon = Template.bind({})
-KInputRightIcon.args = {
-  onChange: (value: string) => {},
-  placeholder: "Placeholder...",
-  rightIcon: TracksIcon,
-  rightIconClick: () => {
-    alert("right icon clicked")
-  }
-}
-
-export const KInputLeftRightIcon = Template.bind({})
-KInputLeftRightIcon.args = {
-  onChange: (value: string) => {},
-  placeholder: "Placeholder...",
-  leftIcon: TracksIcon,
-  rightIcon: TracksIcon,
-  leftIconClick: () => {
-    alert("left icon clicked")
-  },
-  rightIconClick: () => {
-    alert("right icon clicked")
-  }
+  length: 4,
+  isAlphaNumeric: true,
+  sendImmediately: true
 }
--- src/components/KCodeInput/KCodeInput.tsx
+++ src/components/KCodeInput/KCodeInput.tsx
@@ -1,126 +1,116 @@
-import React, { useEffect, useState, KeyboardEvent } from "react"
+import React, { useEffect, useState, KeyboardEvent, useRef } from "react"
 import "../../main.css"
 
-export interface KInputProps {
+export interface KCodeInputProps {
   value: string
+  length: number
   onChange: (value: string) => void
-  onBlur?: (value: string) => void
-  onKeyDown?: (event: KeyboardEvent) => void
   width?: number
   height?: number
-  leftIcon?: string
-  rightIcon?: string
   background?: string
   activeBackground?: string
   borderRadius?: number
   disabled?: boolean
-  placeholder?: string
-  shadowDisabled?: boolean
-  type?: string
-  leftIconClick?: () => void
-  rightIconClick?: () => void
-  accentColor?: string
   hoverBackground?: string
   padding?: string
   gap?: string
-  border?: string
-  boxShadow?: string
-  fontSize?: string
-  iconSize?: string
-  checked?: boolean
+  fontSize?: string,
+  border?: string,
+  sendImmediately?: boolean,
+  isAlphaNumeric?: boolean,
+  inputWidth?: number
+  inputHeight?: number
 }
 
-const KInput: React.FC<KInputProps> = (props) => {
-  const [background, setBackground] = useState("#F5F5F5")
-  const [hover, setHover] = useState(false)
+const KInput: React.FC<KCodeInputProps> = (props) => {
 
-  useEffect(() => {
-    const emptyBackground = props.background || "#F5F5F5"
-    const activeBackground = props.activeBackground || "#FFF"
+  const [focusedIndex, setFocusedIndex] = useState<number>(0)
+  const inputRefs = useRef<HTMLInputElement[]>([])
 
-    const background = props.value ? activeBackground : emptyBackground
-    setBackground(background)
-  }, [props.value])
+
 
   const width = props.width || "100%"
   const height = props.height || 20
   const borderRadius = props.borderRadius || 10
-  const boxShadow = props.shadowDisabled
-    ? ""
-    : props.boxShadow
-    ? props.boxShadow
-    : "0 0 0 1px rgba(17, 17, 17, 0.04), 0 1px 1px 0 rgba(17, 17, 17, 0.04)"
-  const type = props.type || "text"
-  const accentColor = props.accentColor || ""
   const disabled = props.disabled || false
-  const hoverBackground = props.hoverBackground || background
   const padding = props.padding || "8px"
   const gap = props.gap || "12px"
-  const border = props.border || "none"
-  const fontSize = props.fontSize || "14px"
-  const iconSize = props.iconSize || "20px"
+  const fontSize = props.fontSize || "30px"
+  const length = props.length || 6
+  const border = props.border || "1px solid #000"
+  const sendImmediately = props.sendImmediately || false
+  const isAlphaNumeric =  props.isAlphaNumeric  || false  // only numeric for default case
+  const inputWidth = props.inputWidth || "60px"
+  const inputHeight = props.inputHeight || "60px"
+  const background = props.background || "#F5F5F5"
+  
+  const [values, setValues] = useState<string[]>(Array(length).fill(''))
+
+  const handleChange = (text:string, index:number) => {
+
+    if (!isAlphaNumeric && /^\d*$/.test(text)) {
+      const newValues = [...values]
+      newValues[index] = text
+      setValues(newValues)
+      if (!text) {
+        setFocusedIndex(index - 1)
+      } else {
+        setFocusedIndex(index + 1)
+      }
+    }
+    if (isAlphaNumeric) {
+      const newValues = [...values]
+      newValues[index] = text
+      setValues(newValues)
+      if (!text) {
+        setFocusedIndex(index - 1)
+      } else {
+        setFocusedIndex(index + 1)
+      }
+    }
+  }
+  
+  useEffect(() => {
+    // Focus the input at focusedIndex whenever it changes
+    if (inputRefs.current[focusedIndex]) {
+      inputRefs.current[focusedIndex].focus()
+    }
+  }, [focusedIndex])
+
+  useEffect(() => {
+    const allDone = values.every((value) => value.length > 0 )
+    if (sendImmediately && allDone) {
+      console.log(values.join(''))
+    }
+   
+  
+
+  }, [values])
+
+  const renderCharacterComponent = (index:number) => {
+    return (
+      <input key={`k-code-input-${index}`} className="k-code-input-character-container hover:border-['1px solid red']" type="text"
+      style={{
+        background,
+        borderRadius,
+        width: inputWidth,
+        height: inputHeight
+      }}
+      value={values[index]}
+      maxLength={1}
+      onChange={(event) => {
+        handleChange(event.target.value, index)
+      }}
+      ref={(el:HTMLInputElement) => (inputRefs.current[index] = el)}
+      />
+    )
+  }
 
   return (
-    <div
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-      className={"k-input-container"}
-      style={{ background: hover ? hoverBackground : background, borderRadius, boxShadow, padding, gap, border }}
-    >
-      {props.leftIcon && (
-        <img
-          src={props.leftIcon}
-          style={{
-            width: iconSize,
-            height: iconSize
-          }}
-          alt={"l-icon"}
-          className={props.leftIconClick && "cursor-pointer"}
-          onClick={() => {
-            if (props.leftIconClick) props.leftIconClick()
-          }}
-        />
-      )}
-
-      <input
-        type={type}
-        className={"k-input"}
-        style={{
-          background: hover ? hoverBackground : background,
-          width,
-          height,
-          accentColor,
-          fontSize
-        }}
-        {...(props.checked && (type === "checkbox" || type === "radio") && { checked: props.checked })}
-        value={props.value}
-        placeholder={props.placeholder || ""}
-        disabled={disabled}
-        onBlur={(event) => {
-          if (props.onBlur) props.onBlur(event.target.value)
-        }}
-        onChange={(event) => {
-          props.onChange(event.target.value)
-        }}
-        onKeyDown={(event) => {
-          if (props.onKeyDown) props.onKeyDown(event)
-        }}
-      />
-
-      {props.rightIcon && (
-        <img
-          src={props.rightIcon}
-          style={{
-            width: iconSize,
-            height: iconSize
-          }}
-          alt={"r-icon"}
-          className={props.rightIconClick && "cursor-pointer"}
-          onClick={() => {
-            if (props.rightIconClick) props.rightIconClick()
-          }}
-        />
-      )}
+    <div className="flex flex-row justify-between items-center" style={{gap, width: "100%"}}>
+      {Array.from({length}, (_, index) => (
+        renderCharacterComponent(index)
+      ))}
     </div>
   )
 }
