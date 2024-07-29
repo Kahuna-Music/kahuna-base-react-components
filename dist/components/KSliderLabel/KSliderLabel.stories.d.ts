c828a487178bd1be7480fa43e035093ff049c83c
barin <barin.ozmen@xerini.co.uk>
2024-07-29 13:19:22 +0300

    build 0.20

--- dist/components/KSliderLabel/KSliderLabel.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import { SliderLabelProps } from "./KSliderLabel";
-declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react").ReactRenderer, SliderLabelProps>;
-export default _default;
-export declare const KSliderLabelPrimary: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, SliderLabelProps>;
-export declare const KSliderLabelHoverText: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, SliderLabelProps>;
