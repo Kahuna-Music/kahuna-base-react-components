7f0158988169276584a21f7c7b58c3789fbfaa2d
barin <barin.ozmen@xerini.co.uk>
2024-08-26 18:12:15 +0300

    get build

--- dist/components/KSlider/KSlider.stories.d.ts
+++
@@ -1,3 +0,0 @@
-declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react").ReactRenderer, import("./KSlider").KSliderProps>;
-export default _default;
-export declare const KSliderPrimary: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, import("./KSlider").KSliderProps>;
