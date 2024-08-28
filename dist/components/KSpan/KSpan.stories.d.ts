45d3f353b24e03930b75ecbddcdc361336a00321
barin <barin.ozmen@xerini.co.uk>
2024-08-28 11:53:13 +0300

    1.1.5

--- dist/components/KSpan/KSpan.stories.d.ts
+++
@@ -1,4 +0,0 @@
-declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react").ReactRenderer, import("./KSpan").KSpanProps>;
-export default _default;
-export declare const KSpanPrimary: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, import("./KSpan").KSpanProps>;
-export declare const KSpanHoverText: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, import("./KSpan").KSpanProps>;
