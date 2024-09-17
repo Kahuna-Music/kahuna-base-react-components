fd790957cee651a193dbdbcd89c332775f93517e
barin <barin.ozmen@xerini.co.uk>
2024-09-17 10:17:39 +0300

    get build

--- dist/components/KTitleSpan/KTitleSpan.stories.d.ts
+++
@@ -1,3 +0,0 @@
-declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react").ReactRenderer, import("./KTitleSpan").KTitleSpanProps>;
-export default _default;
-export declare const KTitleSpanPrimary: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, import("./KTitleSpan").KTitleSpanProps>;
