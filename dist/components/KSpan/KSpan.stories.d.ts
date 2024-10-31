d11c4ec99e78f1d935eae771909c47ca2bacb834
barin <barin.ozmen@xerini.co.uk>
2024-10-31 10:22:23 +0300

    build prod

--- dist/components/KSpan/KSpan.stories.d.ts
+++
@@ -1,4 +0,0 @@
-import { KSpanProps } from "./KSpan";
-declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react").ReactRenderer, KSpanProps>;
-export default _default;
-export declare const KSpanPrimary: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, KSpanProps>;
--- dist/components/KTextArea/KTextArea.stories.d.ts
+++
@@ -1,6 +0,0 @@
-declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react").ReactRenderer, import("./KTextArea").KTextAreaProps>;
-export default _default;
-export declare const KTextAreaPrimary: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, import("./KTextArea").KTextAreaProps>;
-export declare const KTextAreaLeftIcon: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, import("./KTextArea").KTextAreaProps>;
-export declare const KTextAreaRightIcon: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, import("./KTextArea").KTextAreaProps>;
-export declare const KTextAreaLeftRightIcon: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, import("./KTextArea").KTextAreaProps>;
