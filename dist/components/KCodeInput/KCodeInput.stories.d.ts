91e579f26caa9b852c1d12573d034a67b8cd9602
barin <barin.ozmen@xerini.co.uk>
2024-10-31 15:31:21 +0300

    1.2.3

--- dist/components/KCodeInput/KCodeInput.stories.d.ts
+++
@@ -1,6 +0,0 @@
-import { KCodeInputProps } from "./KCodeInput";
-declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react").ReactRenderer, KCodeInputProps>;
-export default _default;
-export declare const KCodeInputPrimary: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, KCodeInputProps>;
-export declare const KCodeInputSecondary: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, KCodeInputProps>;
-export declare const KCodeInputDefault: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, KCodeInputProps>;
