62953fa1253207385ed14b19e0b35a58e87ffa7b
barin <barin.ozmen@xerini.co.uk>
2024-11-01 14:29:33 +0300

    1.2.4

--- dist/components/KDropdown/KDropdown.stories.d.ts
+++
@@ -1,8 +0,0 @@
-import { KDropdownProps } from "./KDropdown";
-declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react").ReactRenderer, KDropdownProps>;
-export default _default;
-export declare const KDropdownSingle: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, KDropdownProps>;
-export declare const KDropdownMulti: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, KDropdownProps>;
-export declare const KDropdownLeftIcon: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, KDropdownProps>;
-export declare const KDropdownRightIcon: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, KDropdownProps>;
-export declare const KDropdownLeftRightIcon: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, KDropdownProps>;
--- dist/components/KTitleSpan/KTitleSpan.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import { KTitleSpanProps } from "./KTitleSpan";
-declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react").ReactRenderer, KTitleSpanProps>;
-export default _default;
-export declare const KTitleSpanPrimary: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, KTitleSpanProps>;
-export declare const KTitleSpanSecondary: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, KTitleSpanProps>;
