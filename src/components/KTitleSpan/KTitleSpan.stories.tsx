90fd44444391ee103709cb4c8e2959961206398f
unknown <evrentank@gmail.com>
2025-06-16 16:48:47 +0300

    tooltipProps added to KTitleSpan

--- src/components/KTitleSpan/KTitleSpan.stories.tsx
+++ src/components/KTitleSpan/KTitleSpan.stories.tsx
@@ -1,5 +1,6 @@
 import {Meta, StoryFn} from "@storybook/react";
 import KTitleSpan, { KTitleSpanProps } from "./KTitleSpan";
+import KSpan from "../KSpan";
 
 export default {
   title: "ReactComponentLibrary/KTitleSpan",
@@ -21,18 +22,27 @@ const KTitleSpanWrapper: React.FC<KTitleSpanProps> = (args) => {
 }
 
 const Template: StoryFn<typeof KTitleSpanWrapper> = (args) => <KTitleSpanWrapper {...args} />;
-
+const myText = "Hello how are you ne var you nasıl gidiyor?"
 export const KTitleSpanPrimary = Template.bind({});
 KTitleSpanPrimary.args = {
-  text: "Hello how?",
+  text: myText,
   fontSize: 48,
   color: "#111",
   lineHeight: "56px",
   fontStyle: "normal",
   letterSpacing: "-0.48px",
   bold: false,
-  ellipsis: true
-};
+  ellipsis: true,
+  tooltipProps: {
+    width: "150px",
+    position: "bottom",
+    content: (
+      <div className="w-full whitespace-normal">
+        <KSpan text={myText} color="#111" />
+      </div>
+    )
+  }
+}
 
 export const KTitleSpanSecondary = Template.bind({});
 KTitleSpanSecondary.args = {
